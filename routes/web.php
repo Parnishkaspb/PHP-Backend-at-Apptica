1533909fd4dd930faff11f3cd668ee527f3283a4
Bogdan <51113308+Parnishkaspb@users.noreply.github.com>
2023-11-22 16:10:20 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TopCategoryController;
 use App\Http\Controllers\ApiController;
 
-Route::get('/appTopCategory', [TopCategoryController::class, 'show']);
-// Route::middleware(['throttle:5,1', 'log.request'])->get('/appTopCategory', [TopCategoryController::class, 'show']);
+//Route::get('/appTopCategory', [TopCategoryController::class, 'show']);
+Route::middleware(['throttle:5,1', 'log.request'])->get('/appTopCategory', [TopCategoryController::class, 'show']);
 
 
 Route::post('/submit-api-form', [ApiController::class, 'submitForm']);
